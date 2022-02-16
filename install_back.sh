0eacaab00b6bf075c723ddb4c71a3bdb6888127d
Naba Kumar Das <nabad600@gmail.com>
2022-02-16 15:35:26 +0530

    Update install_back.sh

--- install_back.sh
+++ install_back.sh
@@ -48,6 +48,6 @@ echo "Docker composer version check"
 docker-compose --version
 sudo touch /etc/sysctl.d/50-unprivileged-ports.conf
 sudo chown $USER:$USER /etc/sysctl.d/50-unprivileged-ports.conf
-sudo echo "net.ipv4.ip_unprivileged_port_start=0" >> /etc/sysctl.d/50-unprivileged-ports.conf
+sudo echo "net.ipv4.ip_unprivileged_port_start=80" >> /etc/sysctl.d/50-unprivileged-ports.conf
 sudo chown root:root /etc/sysctl.d/50-unprivileged-ports.conf
 sudo sysctl --system
